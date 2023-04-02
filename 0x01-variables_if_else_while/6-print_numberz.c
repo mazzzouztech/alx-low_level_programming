820bcd372075392a1d953eaea0e68bfac87164bd
elmazzouz <elmazzouznet@gmail.com>
2023-04-01 17:40:02 -0700

    i am programming low

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,13 +4,13 @@
 *
 * Return: Always 0 (Success)
 */
-int main(void)
+int main (void)
 {
 int i;
 for (i = 0; i < 10; i++)
 {
-putchar(i + '0');
+putchar (i + '0');
 }
-putchar('\n');
-return(0);
+putchar ('\n');
+return (0);
 }
