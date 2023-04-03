0fade81dee21090c0db14bda202968b1775ebdeb
elmazzouz <elmazzouznet@gmail.com>
2023-04-02 17:27:58 -0700

    i am programming low

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,15 @@
 #include "main.h"
+/**
+* print_alphabet - print all alphabet in lowercase
+*/
 
 void print_alphabet(void)
 {
-    char c;
+char c;
 
-    for (c = 'a'; c <= 'z'; c++)
-    {
-        _putchar(c);
-    }
-    _putchar('\n');
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
