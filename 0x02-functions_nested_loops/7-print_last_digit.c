994f8cdc3e5e353fb4820d8a58215b8fb62bef30
elmazzouz <elmazzouznet@gmail.com>
2023-04-04 16:25:53 -0700

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * print_last_digit - Prints the last digit of a number
- * @n: The number to extract the last digit from
- *
- * Return: The value of the last digit
- */
+* print_last_digit - Prints the last digit of a number
+* @n: The number to extract the last digit from
+*
+* Return: The value of the last digit
+*/
 int print_last_digit(int n)
 {
-    int last_digit;
+int last_digit;
 
-    last_digit = n % 10;
+last_digit = n % 10;
 
-    if (last_digit < 0)
-        last_digit = -last_digit;
+if (last_digit < 0)
+last_digit = -last_digit;
 
-    _putchar('0' + last_digit);
+_putchar('0' + last_digit);
 
-    return last_digit;
+return last_digit;
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,43 +1,38 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table
- */
+* times_table - prints the 9 times table, starting with 0
+*/
 void times_table(void)
 {
-	int row, column, product;
+int i, j, k;
 
-	for (row = 0; row <= 9; row++)
-	{
-		for (column = 0; column <= 9; column++)
-		{
-			product = row * column;
-
-			if (column == 0)
-				_putchar('0');
-			else if (product < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(product + '0');
-			}
-			else if (product >= 10 && product < 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((product / 100) + '0');
-				_putchar(((product / 10) % 10) + '0');
-				_putchar((product % 10) + '0');
-			}
-		}
-		_putchar('\n');
-	}
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+k = j * i;
+if (j == 0)
+{
+_putchar(k + '0');
 }
+
+if (k < 10 && j != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+}
+else if (k >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
+
