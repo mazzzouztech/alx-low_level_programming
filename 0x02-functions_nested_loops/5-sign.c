247fb585bc76ce5b2cdae7b2e160b60be014ebbe
elmazzouz <elmazzouznet@gmail.com>
2023-04-04 16:08:23 -0700

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,26 +2,25 @@
 
 /**
 * print_sign - prints the sign of a number
-* @n: the number to check
-*
-* Return: 1 if n is positive, 0 if n is zero, -1 if n is negative
+* @n: the int to check
+* Return: 1 for positive num, -1 for negative num or zero for anything else.
 */
-
 int print_sign(int n)
 {
-if (n > 0)
-{
-_putchar('+');
-return (1);
-}
-else if (n == 0)
-{
-_putchar('0');
-return (0);
-}
-else
-{
-_putchar('-');
-return (-1);
-}
+
+            if (n > 0)
+            {
+            _putchar(43);
+            return (1);
+            } 
+            else if (n < 0)
+            { 
+            _putchar(45);
+            return (-1);
+            } 
+            else 
+            {
+            _putchar(48);
+            return (0);
+            }
 }
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,13 @@
 /**
- * _abs - Computes the absolute value of an integer
- * @n: The integer to compute the absolute value of
- *
- * Return: The absolute value of n
- */
+* _abs - Computes the absolute value of an integer
+* @n: The integer to compute the absolute value of
+*
+* Return: The absolute value of n
+*/
 int _abs(int n)
 {
-    if (n < 0)
-        return (-n);
-    else
-        return (n);
+if (n < 0)
+return (-n);
+else
+return (n);
 }
